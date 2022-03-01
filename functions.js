a137b9d3980986c914f596daa795519bb98d94d4
Ezequiel Rico <ezequielrico@outlook.com>
2022-03-01 01:19:06 -0300

    menú hamburguesa ok y nuevo top.png

--- functions.js
+++ functions.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
     $('#menu_on').click(function(){
     	$('body').toggleClass('visible_menu');
     })
+    $('#menu_off').click(function(){
+      $('body').toggleClass('visible_menu');
+    })  
 });
 $(document).ready(function(){ irArriba(); });
 
